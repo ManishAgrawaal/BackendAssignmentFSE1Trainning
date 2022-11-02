94edffdc82bedd1806693bd3d0470f056e95d516
unknown <cogdotnet1597@iiht.tech>
2022-11-02 22:34:52 +0530

    first commit

---
+++ Shopping/ViewModel/AddProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Shopping.ViewModel
+{
+    public class AddProduct
+    {
+        public string Productname { get; set; }
+        public string Productdesc { get; set; }
+        public string Price { get; set; }
+        public string Features { get; set; }
+        [JsonIgnore]
+        public int Status { get; set; }
+    }
+}
